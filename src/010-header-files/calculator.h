fe59862d0adb0bba9a5e093e786b7f65da04bf80
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2024-05-07 22:19:29 +0200

    add header guard

--- src/010-header-files/calculator.h
+++ src/010-header-files/calculator.h
@@ -1,2 +1,11 @@
+/*
+    This is a header file.
+    The purpose of a header file is to provide information to the compiler about the symbols used in the program.
+    We also use header guard to prevent multiple inclusions of the same header file in the same translation unit.
+    The header guard is defined by the preprocessor directive #ifndef, #define, and #endif.
+*/
+#ifndef CALCULATOR_H
+    #define CALCULATOR_H
 // propagate forward declaration of add function
 int add(int x, int y);
+#endif

1c94e99b72c3a7414a8a9d988da7c55e557b626f
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2024-05-09 01:52:13 +0200

    update namespaces

--- src/008-namespaces/main.cpp
+++ src/008-namespaces/main.cpp
@@ -10,6 +10,29 @@ int x;
 // non-const global variable definition (with initializer)
 int y { 5 };
 
+// we can create our own namespaces to avoid naming collisions
+namespace Something
+{
+    void say_hello()
+    {
+        std::cout << "Hello from Something namespace!" << endl;
+    }
+
+    // we can nest namespaces
+    namespace Inner
+    {
+        void say_hello()
+        {
+            std::cout << "Hello from Inner namespace!" << endl;
+        }
+    }
+}
+
+void say_hello()
+{
+    std::cout << "Hello from global namespace!" << endl;
+}
+
 int main()
 {
     // cout is a member of the std namespace
@@ -17,5 +40,15 @@ int main()
     // when the namespace is added as prefix, we call this fully qualified name
     // fully qualified name is used to avoid ambiguity (and confusion) when two namespaces have members with the same name
     std::cout << "Hello world!";
+    
+    // using scope resolution operator without the namespace prefix will call the member from the global namespace
+    Something::Inner::say_hello();
+    Something::say_hello();
+    ::say_hello();
+
+    // we can alias namespaces
+    namespace S = Something;
+    S::say_hello();
+
     return 0;
 }
